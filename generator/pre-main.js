d910f86d125b9dbb9c409295527385e556c5440c
tianyiming26@gmail.com <tianyiming26@gmail.com>
2018-08-31 18:38:36 +0800

    Upgrade version to 0.2.0

--- generator/pre-main.js
+++ generator/pre-main.js
@@ -1,5 +1,6 @@
 module.exports = (api, opts = {}) => {
   api.injectImports(api.entryFile, `import Element from 'element-ui';`);
+  api.injectImports(api.entryFile, `import 'element-ui/lib/theme-chalk/index.css';`);
 
   api.render('./template');
 }
