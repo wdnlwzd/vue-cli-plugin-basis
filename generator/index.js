8ef2af4a83b00418bdce97dd608d078a655f5c22
tianyiming26@gmail.com <tianyiming26@gmail.com>
2018-08-31 11:35:12 +0800

    add: The generator folder

---
+++ generator/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+  api.extendPackage({
+    dependencies: {
+      'element-ui': '^2.4.6',
+    },
+  });
+
+  api.render('./templates');
+}
---
+++ generator/templates/src/views/Index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h2>Hello, welcome to come here.</h2>
+  </div>
+</template>
