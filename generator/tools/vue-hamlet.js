e1c5b9709b1efb01db338172757a1608238c5857
tianyiming26@gmail.com <tianyiming26@gmail.com>
2018-09-04 11:59:10 +0800

    add: vuex-router-sync

--- generator/tools/vue-hamlet.js
+++ generator/tools/vue-hamlet.js
@@ -1,14 +1,23 @@
 const helpers = require('../tools/helpers');
 
+function addAbsoluteImports(lines) {
+  let lastImportIndex = lines.findIndex(line => line.match(/^import Vue/));
+
+  lines.splice(lastImportIndex += 1, 0, `import { sync } from 'vuex-router-sync';`);
+  return lines;
+}
+
 module.exports = (api, opts, files) => {
   api.extendPackage({
     dependencies: {
       'vue-hamlet': 'github:yimian/vue-hamlet#v1.0.2',
+      'vuex-router-sync': '^5.0.0',
     }
   });
   api.injectImports(api.entryFile, `import auth from '@/hamlet';`);
   api.onCreateComplete(() => {
     helpers.updateFile(api, api.entryFile, lines => {
+      lines = addAbsoluteImports(lines);
       lines.reverse();
       let lastImportIndex = lines.findIndex(line => line.match(/^import/));
       lines[lastImportIndex] += `\n`;
@@ -23,6 +32,7 @@ Vue.use(auth, {
   // fetchUser: '/api/common/users/me',
 });
 `;
+      lines[lastImportIndex] += `\nsync(store, router);`;
       lines.reverse().join('\n');
       return lines;
     });
