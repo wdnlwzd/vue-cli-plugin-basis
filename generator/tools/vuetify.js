c6f79970df2634fca62f672c4c6c6bd2ad30c1c7
tianyiming26@gmail.com <tianyiming26@gmail.com>
2018-09-13 18:14:46 +0800

    add: vuetify

---
+++ generator/tools/vuetify.js
@@ -0,0 +1,33 @@
+const helpers = require('../utils/helpers');
+const polyfill = require('../utils/polyfill');
+const fonts = require('./fonts');
+
+function addAbsoluteImports(lines) {
+  let lastImportIndex = lines.findIndex(line => line.match(/^import Vue/));
+  lines.splice(lastImportIndex += 1, 0, `import '@babel/polyfill';`);
+  return lines;
+}
+
+module.exports = (api, opts) => {
+  api.extendPackage({
+    dependencies: {
+      'vuetify': '^1.2.3',
+    }
+  });
+
+  polyfill.addDependencies(api);
+  opts.installFonts && fonts.addDependencies(api, opts.iconFont);
+  opts.installFonts && fonts.addImports(api, opts.iconFont);
+
+  api.injectImports(api.entryFile, `import './plugins/vuetify';`);
+  api.render({
+    ['./src/plugins/vuetify.js']: '../plugins/template/src/plugins/vuetify.js',
+  }, { opts });
+
+  api.onCreateComplete(() => {
+    polyfill.updateBabelConfig(api);
+    polyfill.updateBrowsersList(api);
+    polyfill.addImports(api);
+    !opts.installFonts && fonts.addLinks(api, opts.iconFont);
+  });
+};
