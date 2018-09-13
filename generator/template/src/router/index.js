11cc21ffdc450217b515a9890bc4f0158e8d496e
tianyiming26@gmail.com <tianyiming26@gmail.com>
2018-09-13 15:55:22 +0800

    mod: router

--- generator/template/src/router/index.js
+++ generator/template/src/router/index.js
@@ -6,9 +6,25 @@ import Homepage from '@/views/admin/Homepage.vue';
 
 Vue.use(Router);
 
-// const Root = Vue.component('temp', {
-//   template: '<router-view></router-view>',
-// });
+const Root = Vue.component('temp', {
+  template: '<router-view></router-view>',
+});
+
+const Create = {
+  template:`<div class="title">Create</div>`
+};
+
+const Read = {
+  template:`<div class="title">Read</div>`
+};
+
+const Update = {
+  template:`<div class="title">Update</div>`
+};
+
+const Delete = {
+  template:`<div class="title">Delete</div>`
+};
 
 export default new Router({
   mode: 'history',
@@ -30,7 +46,61 @@ export default new Router({
           path: '/homepage',
           name: 'Homepage',
           component: Homepage,
-          meta: {},
+          meta: {
+            icon: 'home',
+          },
+        },
+        {
+          path: '/users',
+          name: 'Users',
+          component: {
+            template:`<div class="title">Users</div>`,
+          },
+          meta: {
+            icon: 'people_outline',
+          },
+        },
+        {
+          path: '/actions',
+          name: 'Actions',
+          component: Root,
+          meta: {
+            hasSub: true,
+          },
+          children: [
+            {
+              path: '/create',
+              name: 'Create',
+              component: Create,
+              meta: {
+                icon: 'add',
+              },
+            },
+            {
+              path: '/read',
+              name: 'Read',
+              component: Read,
+              meta: {
+                icon: 'insert_drive_file',
+              },
+            },
+            {
+              path: '/update',
+              name: 'Update',
+              component: Update,
+              meta: {
+                icon: 'update',
+              },
+            },
+            {
+              path: '/delete',
+              name: 'Delete',
+              component: Delete,
+              meta: {
+                icon: 'delete',
+              },
+            },
+          ],
         },
       ],
     },
