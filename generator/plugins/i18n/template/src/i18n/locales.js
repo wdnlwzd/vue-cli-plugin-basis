a2765017b972dee4ccedb048e5dfe00158898e2c
tianyiming26@gmail.com <tianyiming26@gmail.com>
2018-09-28 14:58:00 +0800

    mod: change `zh` to `zh-CN`

--- generator/plugins/i18n/template/src/i18n/locales.js
+++ generator/plugins/i18n/template/src/i18n/locales.js
@@ -2,161 +2,161 @@ export default {
   common: {
     save: {
       en: 'Save',
-      zh: '保存',
+      'zh-CN': '保存',
     },
     query: {
       en: 'Query',
-      zh: '查询',
+      'zh-CN': '查询',
     },
     reset: {
       en: 'Reset',
-      zh: '重置',
+      'zh-CN': '重置',
     },
     filter: {
       en: 'Filter',
-      zh: '条件筛选',
+      'zh-CN': '条件筛选',
     },
     username: {
       en: 'Username',
-      zh: '用户名',
+      'zh-CN': '用户名',
     },
     email: {
       en: 'Email',
-      zh: '邮箱',
+      'zh-CN': '邮箱',
     },
     role: {
       en: 'Role',
-      zh: '角色',
+      'zh-CN': '角色',
     },
     password: {
       en: 'Password',
-      zh: '密码',
+      'zh-CN': '密码',
     },
     repeat_password: {
       en: 'Input password again',
-      zh: '确认密码',
+      'zh-CN': '确认密码',
     },
     old_password: {
       en: 'Old Password',
-      zh: '旧密码',
+      'zh-CN': '旧密码',
     },
     reset_password: {
       en: 'Reset Password',
-      zh: '重置密码',
+      'zh-CN': '重置密码',
     },
     loginN: {
       en: 'Login',
-      zh: '登录',
+      'zh-CN': '登录',
     },
     login: {
       en: 'Log In',
-      zh: '登录',
+      'zh-CN': '登录',
     },
     logout: {
       en: 'Log Out',
-      zh: '退出登录',
+      'zh-CN': '退出登录',
     },
     account: {
       en: 'Account',
-      zh: '修改密码',
+      'zh-CN': '修改密码',
     },
     forgetPassword: {
       en: 'Forget Password?',
-      zh: '忘记密码?',
+      'zh-CN': '忘记密码?',
     },
     recoverPassword: {
       en: 'Recover password?',
-      zh: '找回密码',
+      'zh-CN': '找回密码',
     },
     edit: {
       en: 'Edit',
-      zh: '编辑',
+      'zh-CN': '编辑',
     },
     delete: {
       en: 'Delete',
-      zh: '删除',
+      'zh-CN': '删除',
     },
     ok: {
       en: 'OK',
-      zh: '确定',
+      'zh-CN': '确定',
     },
     cancel: {
       en: 'Cancel',
-      zh: '取消',
+      'zh-CN': '取消',
     },
     random_password: {
       en: 'Random Password',
-      zh: '随机密码',
+      'zh-CN': '随机密码',
     },
     return: {
       en: 'Return',
-      zh: '返回',
+      'zh-CN': '返回',
     },
     invalid_password_username: {
       en: 'Invalid password or username',
-      zh: '用户名或密码错误！',
+      'zh-CN': '用户名或密码错误！',
     },
     invalid_password_username_number: {
       en: 'Invalid Username or Password. {number} logins Remained.',
-      zh: '用户名或密码错误, 剩余登录次数{number}次！',
+      'zh-CN': '用户名或密码错误, 剩余登录次数{number}次！',
     },
     account_is_locked: {
       en: 'Account Locked! Please Contact Administrator to Unlock.',
-      zh: '您的账号已被锁定，请联系管理员！',
+      'zh-CN': '您的账号已被锁定，请联系管理员！',
     },
     change_fail: {
       en: 'Password change failed, please try again!',
-      zh: '密码修改失败, 请重试',
+      'zh-CN': '密码修改失败, 请重试',
     },
     change_success: {
       en: 'Password change successfully!',
-      zh: '密码已修改',
+      'zh-CN': '密码已修改',
     },
     reset_fail: {
       en: 'Password reset failed, please try again!',
-      zh: '密码重置失败, 请重试',
+      'zh-CN': '密码重置失败, 请重试',
     },
     reset_success: {
       en: 'Password reset successfully!',
-      zh: '密码已重置',
+      'zh-CN': '密码已重置',
     },
     notBindApp: {
       en: 'not bind to current app',
-      zh: '对不起，您还没有获得权限，请联系管理员',
+      'zh-CN': '对不起，您还没有获得权限，请联系管理员',
     },
     networkError: {
       en: 'Network Error',
-      zh: '网络错误',
+      'zh-CN': '网络错误',
     },
     copyrightMessage: {
       en: '© 2014 - {currentYear} Shenzhen Yimian Network Technology Co.,Ltd.',
-      zh: '© 2014 - {currentYear} 深圳市一面网络技术有限公司 粤 ICP 备 14054704 号 - 4',
+      'zh-CN': '© 2014 - {currentYear} 深圳市一面网络技术有限公司 粤 ICP 备 14054704 号 - 4',
     },
     all: {
       en: 'All',
-      zh: '全部',
+      'zh-CN': '全部',
     },
   },
   rules: {
     require_password: {
       en: 'Password is required.',
-      zh: '请填写密码',
+      'zh-CN': '请填写密码',
     },
     password_format: {
       en: 'Your password should contain at least 8 characters including a number, one lowercase letter and one uppercase letter',
-      zh: '密码必须包含至少一个小写字母、一个大写字母和一个数字，长度至少八位。',
+      'zh-CN': '密码必须包含至少一个小写字母、一个大写字母和一个数字，长度至少八位。',
     },
     password_placeholder: {
       en: 'Input password',
-      zh: '请输入密码',
+      'zh-CN': '请输入密码',
     },
     password_repeat: {
       en: 'The password and its confirm are not the same.',
-      zh: '两次密码输入不一致。',
+      'zh-CN': '两次密码输入不一致。',
     },
     username_placeholder: {
       en: 'Input username',
-      zh: '请输入用户名',
+      'zh-CN': '请输入用户名',
     },
   },
 };
